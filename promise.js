d729449ff61c42d45d17a065904fa2cfceb86f1a
ZhouyiGuan <ZhouyiGuan@ZD-automotive.de>
2023-05-04 11:47:30 +0200

    add serialport.js

--- promise.js
+++ promise.js
@@ -1,53 +1,57 @@
 //函数内定义promise 使得函数异步执行 
-let A = 2;
-function functionA(){
-    return new Promise((resolve,reject) => {//Promise中有一个函数，并向里面传两个参数
-        console.log("functionA start......");
-        if (A < 5){
-            resolve();
-        }else{
-            reject();
-        }
-    })
-    .then(() => {//成功时执行这个函数
-        console.log("func success");    
-    })
-    .then(function () {//then,catch接受链式
-        console.log("func success2");
-    })
-    .catch(() => {//失败时执行这个函数 且catch只能有一个 用于捕捉reject错误
-        console.log("functionA failed");
-    })
-    .finally(() => {//最终执行
-        console.log("functionA finished......");
-    })
-}
 
 let B = 2;
 function functionB(){
-    return new Promise((resolve,reject) => {//这两个参数也可以传递参数
+    return new Promise((resolve,reject) => {//Promise中有一个函数，并向里面传两个参数 ,这两个参数也可以传递参数
         console.log("functionB start......");
         if (B < 5){
-            resolve(111);
+            resolve(1);
         }else{
-            reject(999);
+            reject(9);
         }
     })
     .then((value) => {
-        console.log("value1:",value);    
-        return 222;
+        console.log("value1:",value);//成功时执行这个函数
+        return 2;
     })
     .then((value) => {
-        console.log("value2:",value);
-        return 333;
+        console.log("value2:",value);//then,catch接受链式
+        return 3;
     })
     .catch((value) => {
-        console.log("failed value:",value);
+        console.log("failed value:",value);//失败时执行这个函数 且catch只能有一个 用于捕捉reject错误
     })
     .finally(() => {
-        console.log("functionB finished......");
+        console.log("functionB finished......");//无论进入then还是catch 都会执行finally
     })
 }
 
-functionA();
 functionB();
+
+
+
+const varB = new Promise((resolve,reject) => {
+        console.log("varB creat......");
+        if (B < 5){
+            resolve(11);
+        }else{
+            reject(99);
+        }
+});
+varB.then((value) => {
+    console.log("value1:",value);    
+    return 22;
+})
+.then((value) => {
+    console.log("value2:",value);
+    return 33;
+})
+.catch((value) => {
+    console.log("failed value:",value);
+})
+.finally(() => {
+    console.log("varB finished......");
+});
+
+//varB这里在创建变量时，创建promise函数 然后执行；；
+//而functionB是先定义，只有在运行的时候才去创建一个promise函数；；
--- serialPort.js
+++ serialPort.js
@@ -1,10 +1,48 @@
-const serialPort = require("serialport");
+const { SerialPort } = require("serialport");//这个大括号必不可少 不知道为什么他们的代码可行 但是这里只有大括号可以跑
 
-serialPort.list()
-.then(( ports ) => {
-    console.log(ports);
-} )
-.catch(( error ) => {
-    console.log(error);
+//返回所有可用串口    .list函数是一个返回promise的函数 
+SerialPort.list().then((ports) => {
+    console.log(ports); 
+}).catch((err) => {
+    console.log(err);
 })
 
+//设置串口
+const port0 = new SerialPort({//设置成手动打开
+  path: '/dev/ttyUSB11',
+  baudRate: 115200,
+  autoOpen: false,
+})
+const port1 = new SerialPort({//也可以设置成自动打开，同时接受回调函数确认是否打开
+    path: "/dev/ttyUSB12",
+    baudRate: 115200,
+}, (error) => {
+    if(error){
+        console.log("open fail with message: ",error.message);
+    }
+})
+
+//手动打开串口
+port0.open(function (err) {
+    if (err) {
+        return console.log('Error opening port: ', err.message)
+    }
+})
+
+//on可以注册事件 open事件表示在串口打开时触发;;data表示串口收到数据时触发;;
+port0.on('open', () => {
+    console.log("port open.");
+})
+port0.on('close', () => {
+    console.log('port close.');
+})
+port0.on('data', (data) => {//传入data的方式读取收到的数据
+    console.log('Received data:', data);
+})
+port0.on('readable', function () {//用read方式读取收到的数据
+    console.log('Received data:', port0.read())
+})
+
+//write可以向串口输出数据
+port0.write('write string')
+port0.write(Buffer.from('write buffer'))
